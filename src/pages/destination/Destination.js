ee23b29dae135fc76f1f465b47bdaffbb9e387ba
Felix Criollo <felixcriollovigo@gmail.com>
2022-01-09 19:09:37 -0500

    fix layouts

--- src/pages/destination/Destination.js
+++ src/pages/destination/Destination.js
@@ -1,5 +1,28 @@
+// import { useEffect, useState } from 'react';
+// import { destinationsData } from '../../services/pageDatas';
+import SubTitle from '../../Components/SubTitle';
+
 export default function Destination() {
+  // const [data, setData] = useState([]);
+
+  // useEffect(function () {
+  //   destinationsData()
+  //     .then(destiny => setData(destiny[0]));
+  // }, [])    
+
+  // console.log(data.images.png);
+  // const {name, image, description, distance, travel } = data;
+
   return (
-    <p>Cuerpo Destino</p>
+    <div className="container destination">
+      <div className="top">
+        <SubTitle text={"PICK YOUR DESTINATION"} number={"01"}/>
+
+        <img src={require("../../assets/destination/image-moon.png")} alt=""/>
+      </div>
+
+      <div className="bottom">
+      </div>
+    </div>
   )
 }
--- src/services/pageDatas.js
+++ src/services/pageDatas.js
@@ -1,15 +1,15 @@
 import data from "./data"
 
 // Returns a object with the crew data of the page
-export function  crewData () {
+export function crewData () {
   return data().then(data => data.crew)
 } 
 // Returns a object with the technologies data of the page
-export function  technologyData () {
+export function technologyData () {
   return data().then(data => data.technology)
 } 
 // Returns a object with the destinations data of the page
-export function  destinationsData () {
+export function destinationsData () {
   return data().then(data => data.destinations)
 } 
 
